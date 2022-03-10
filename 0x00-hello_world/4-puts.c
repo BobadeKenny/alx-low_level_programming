508a55cbf26a6aff8263911ea5ec8e077371942e
Kenny <kennygbeminiyi@gmail.com>
2022-03-11 00:44:22 +0100

    c file corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 /**
-* A main function 
-*/
+ * main - This is the entry point of the project
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
