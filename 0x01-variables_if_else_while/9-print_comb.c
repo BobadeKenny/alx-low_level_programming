7b908fc4058c38af15681e4043c6b953e61cf62a
Kenny <kennygbeminiyi@gmail.com>
2022-03-11 23:31:07 +0100

    test commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the project
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+    int a;
+    for (a = 0; a<=9; a++)
+    {
+        if (a == 9)
+        {
+            putchar(a);
+        }
+        else 
+        {
+            putchar(a);
+            putchar(",");
+            putchar(" ");
+        }
+        
+    }
+        return (0);
+}
