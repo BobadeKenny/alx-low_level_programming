59b9141deaff51837e699e2ee8f52a118147ec67
Kenny <kennygbeminiyi@gmail.com>
2022-03-12 00:41:48 +0100

    corrections and 1 and 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,23 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-    int n;
+int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    /* your code goes there */
-    return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+printf(n);
+if (n > 0)
+{
+printf(" is positive");
+}
+if (n < 0)
+{
+printf(" is negative");
+}
+if (n == 0)
+{
+printf(" is zero");
+}
+return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,10 +5,28 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-    int n;
+int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    /* your code goes there */
-    return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+int last = n % 10;
+
+printf("Last digit of ");
+printf(n);
+printf(" is ");
+printf(last);
+if (last > 5)
+{
+printf(" and is greater than 5");
+}
+if (last == 0)
+{
+printf(" and is 0");
+}
+if (last < 6 && last != 0)
+{
+    printf(" and is less than 6 and not 0");
+}
+return (0);
 }
