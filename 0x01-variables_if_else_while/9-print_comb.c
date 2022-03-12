4677752114ea97cdbceb8e91b99264ef4478d9ba
Kenny <kennygbeminiyi@gmail.com>
2022-03-12 01:15:52 +0100

    better work

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,16 +12,13 @@ int a;
 
 for (a = '0'; a <= '9'; a++)
 {
-if (a == '9')
-{
 putchar(a);
-}
-else 
+if (a != '9')
 {
-putchar(a);
 putchar(',');
 putchar(' ');
-}    
 }
+}
+putchar('\n');
 return (0);
 }
