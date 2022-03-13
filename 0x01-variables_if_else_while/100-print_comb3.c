e54b909012ff2e9609d6c56bf8a7d2ce85ce53b6
Kenny <kennygbeminiyi@gmail.com>
2022-03-13 08:17:32 +0100

    corrections plus advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the project
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+int a;
+int b = 0;
+
+while (b < 10)
+{
+a = 0;
+while (a < 10)
+{
+if (b != a && b < a)
+{
+putchar('0' + b);
+putchar('0' + a);
+
+if (a + b != 17)
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+a++;
+}
+b++;
+}
+putchar('\n');
+return (0);
+}
