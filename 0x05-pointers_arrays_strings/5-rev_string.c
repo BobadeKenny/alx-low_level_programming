a238af33aa7494e095e7f504aba0a76fb5cf5f9f
Kenny <kennygbeminiyi@gmail.com>
2022-03-22 00:25:25 +0100

    correction

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,15 +6,21 @@
 */
 void rev_string(char *s)
 {
-	int i = 0;
-	while (s[i])
+	int len, i, half;
+	char temp;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+
+	i = 0;
+
+	half = len / 2;
+
+	while (half--)
 	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
 		i++;
 	}
-
-	while (i--)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
 }
