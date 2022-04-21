4811a7ff8fb9eaea30a1a8f59716a5349210213a
Kenny <kennygbeminiyi@gmail.com>
2022-04-22 00:59:05 +0100

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+	list_t *nxt;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		nxt = current->next;
+		free(current);
+		current = nxt;
+	}
+}
