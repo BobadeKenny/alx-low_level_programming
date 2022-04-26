6adc9d5b6e19ab89b63163cd5587a26a1ead744f
Kenny <kennygbeminiyi@gmail.com>
2022-04-26 21:05:55 +0100

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: head
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+	listint_t *nxt;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		nxt = current->next;
+		free(current->n);
+		free(current);
+		current = nxt;
+	}
+}
