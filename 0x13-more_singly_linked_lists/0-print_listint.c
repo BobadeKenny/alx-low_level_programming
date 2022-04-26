7dd8fb00284f9ddbdf90d0347ca5dccafcca722a
Kenny <kennygbeminiyi@gmail.com>
2022-04-26 20:44:26 +0100

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements in list
+ * @h: list
+ *
+ * Return: num of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *node = h;
+	size_t i = 0;
+
+	while (node != NULL)
+	{
+		printf("%i\n", node->n);
+		i++;
+		node = node->next;
+	}
+	return (i);
+}
