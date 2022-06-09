3206c365cf0bd36082220dbfc597a3592aeed7c7
Kenny <kennygbeminiyi@gmail.com>
2022-06-09 17:15:23 +0100

    6 and 7

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums all nodes
+ * @head: pointer to list
+ * Return: node
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    dlistint_t *current;
+    unsigned int result;
+
+    current = head;
+    result = 0;
+    if (current == NULL)
+    {
+        return(0);
+    }
+    while(current != NULL)
+    {
+        result += current->n;
+        current = current->next;
+    }
+    return(result);
+}
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - gets node at index
+ * @h: pointer to list
+ * @idx: index of node
+ * @n: new node value
+ * Return: node address
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+    dlistint_t *current;
+    dlistint_t *new;
+    unsigned int i;
+
+    current = *h;
+    new = malloc(sizeof(dlistint_t));
+    if (new == NULL)
+    {
+        free(new);
+        return (NULL);
+    }
+
+    new->n = n;
+    if (current == NULL)
+    {
+        return(NULL);
+    }
+    for(i = 0; i < idx; i++)
+    {
+        current = current->next;
+        if (current == NULL)
+        {
+            return(NULL);
+        }
+    }
+    new->prev = current->prev;
+    new->next = current;
+    *h = new;
+    return(new);
+}
