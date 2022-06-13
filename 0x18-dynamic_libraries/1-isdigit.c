fe1d692533b629dbea9a7ed11a79559311b5930b
Kenny <kennygbeminiyi@gmail.com>
2022-06-13 16:51:19 +0100

    0x18 task 0

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c : character to check alphabet
+ * Return:0 or 1
+ */
+int _isalpha(int c)
+{
+return (c >= 65 && c <= 122);
+}
