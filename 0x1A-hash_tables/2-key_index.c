df42932ff9f3f317ea9c3d80e0e72eb6e9e79eb3
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-07-01 16:18:34 +0100

    corrections

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - return key index
+ * @key: key
+ * @size: size of array
+ * Return: index of key
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
     return (hash_djb2(key) % size);
